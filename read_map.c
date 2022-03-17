95be3aa9b05e066110f8906713bcd042dca4747e
Ran LIU <rliu@e1r11p7.42.fr>
2022-03-17 16:30:32 +0100

    0318

--- read_map.c
+++ read_map.c
@@ -6,10 +6,30 @@
 /*   By: rliu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 15:11:50 by rliu              #+#    #+#             */
-/*   Updated: 2022/03/16 18:49:01 by rliu             ###   ########.fr       */
+/*   Updated: 2022/03/17 16:29:51 by rliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
+
+struct	mapcharacters
+{
+	int wall;
+	int collect;
+	int start;
+	int exit;
+	int place;
+} mapcharacter;
+
+void	ft_map_exit(char *message, t_map *map)
+{
+	ft_putstr_fd(message, 1);
+	if (map->maplist)
+		free(map->maplist);
+	if (map->maparray)
+		free(map->maparray);
+	free(map);
+	exit (1);
+}
 
 size_t	ft_strlen_non(char *str)
 {
@@ -21,122 +41,124 @@ size_t	ft_strlen_non(char *str)
 	return(len);
 }
 
-int ft_checkmap_size(t_map *map, char *map_path)
+int ft_checkmap_shape(t_map *map, int fd)
 {
-	int		fd;
 	char	*temp;
 
 	temp = 0;
-	fd = open(map_path, O_RDONLY);
-	if (fd < 0)
-	{
-		printf("File error: fd does not exist!");
-		exit (1);
-	}
+	map->maplist = ft_strdup("");
 	while (fd)
 	{
 		temp = get_next_line(fd);
-		if (!temp )
+		if (!temp)
 		{
-			if (!map->mapcol)
-			{
-				printf ("empty map or read map fail\n");
-				return(0);
-			}
-			return(1);
+			if (map->mapcol >= 4 && map->mapline >= 4)
+				return (1);
+			ft_map_exit("Error\nMap size should be at least 4 x 4", map); 
 		}
-//		printf("%s", temp);
-//		free(temp);
-//		else
-//		{
-		if (map->mapcol == 0)
-		{
+		else if (!ft_strlen_non(temp))
+			ft_map_exit("Error\nEmpty line", map);
+		else if (map->mapcol == 0)
 			map->mapcol = ft_strlen_non(temp);
-			map->maplist = ft_strjoin_free(map->maplist, temp);
-		}
-		else if (map->mapcol == ft_strlen_non(temp))
-			map->maplist = ft_strjoin_free(map->maplist, temp);
-		else
-		{
-				printf("not right size");	
-				return(0);
-		}
-			map->mapline += 1;
-//		}
+		else if (map->mapcol != ft_strlen_non(temp))
+			ft_map_exit("Error\nMap size should be rectangle", map);
+		map->maplist = ft_strjoin_free(map->maplist, temp);
+		map->mapline += 1;
 	}
 	return (0);
 }
 
-int ft_checkmap_c(t_map *map)
+void	ft_checkmap_character_count(char c, t_map *map)
+{	
+	if (c == '1')
+		mapcharacter.wall++;
+	else if (c =='P')
+	    mapcharacter.start++;
+	else if (c =='E')
+		mapcharacter.exit++;
+	else if (c == 'C')	
+		mapcharacter.collect++;
+	else if (c == '0')
+		mapcharacter.place++;
+	else
+		ft_map_exit("Error\n Invalide character", map);
+}
+
+int ft_checkmap_character_size(t_map *map)
+{
+	if (mapcharacter.start < 1)
+		ft_map_exit("Error\nAt least one depart!", map);
+	if (mapcharacter.start > 1)
+		ft_map_exit("Error\nAt most one depart!", map);
+	if (mapcharacter.exit < 1)
+		ft_map_exit("Error\nAt least one exit!", map);
+	if (mapcharacter.collect < 1)
+		ft_map_exit("Error\nAt least one collect!", map);
+	return (1);
+}
+
+int ft_checkmap_character(t_map *map)
 {
 	int i;
 	int j;
-	int depart = 0;
-	int exit = 0;
-	int collect = 0;
 
 	i = 0;
-	map->maparray = ft_split(map->maplist,'\n');
-	if (!map->maparray)
-		return (0);
 	while (i < map->mapline)
 	{
 		j = 0;
 		while (j < map->mapcol)
 		{
-			if (map->maparray[i][j] != 'E' && map->maparray[i][j] != '1' 
-					&& map->maparray[i][j] != '0' && map->maparray[i][j] != 'P' && map->maparray[i][j] !='C')
-			{
-				printf("Error\n the character is not defined");
-				return (0);
-			}
-			else if ((i == 0 || i == map->mapline - 1 || j == 0 || j == map->mapcol - 1) 
-				&& map->maparray[i][j] != '1')
-			{
-				printf("Error\n this place should be the wall");
-				return (0);
-			}
-			else if (map->maparray[i][j] == 'P')
-				depart++;
-			else if (map->maparray[i][j] == 'E')
-				exit++;
-			else if (map->maparray[i][j] == 'C')
-				collect++;
+			ft_checkmap_character_count(map->maparray[i][j], map);
+			if ((i == 0 || i == map->mapline - 1 || j == 0 || j == map->mapcol - 1)
+					&& map->maparray[i][j] != '1')
+				ft_map_exit("Error\n 4 sides of map should be the wall", map);
 			j++;
 		}
 		i++;
 	}
-	if (!depart || !exit || !collect)
-	{
-		printf("Error\n at least one item, one exit, and one collect, please check your map!");
-		return (0);
-	}
-	return (1);
+	if (ft_checkmap_character_size(map))
+		return (1);
+	return (0);
 }
 
-/*int	main(int argc, char **argv)
+int ft_checkmap(t_map *map, char *map_path)
+{
+	int		fd;
+
+	fd = open(map_path, O_RDONLY);
+	if (fd < 0)
+		ft_map_exit("Error\n Invalid file\n", map);
+	else if (ft_checkmap_shape(map,fd))
+	{
+		map->maparray = ft_split(map->maplist,'\n');  // split the mapstring to one array;
+		if (!map->maparray)
+			ft_map_exit("Error\nMaparray invalid", map); 
+		if (ft_checkmap_character(map))
+			return (1);
+	}
+	return(0);
+}
+
+int	main(int argc, char **argv)
 {
 	t_map	*map;
 
 	map = ft_calloc(1,sizeof(t_map));
-	
-	map->maplist = ft_strdup("");
-	ft_checkmap_size(map, argv[1]);
-	ft_checkmap_c(map);
-	printf("finalt:%s col:%d line:%d\n", map->maplist, map->mapcol, map->mapline);
-
-	int i=0;
-	while(map->maparray[i])
+	if (ft_checkmap(map,argv[1]))
 	{
-		int j=0;
-		while (map->maparray[i][j])
+		printf("finalt:%s col:%d line:%d\n", map->maplist, map->mapcol, map->mapline);
+		int i=0;
+		while(map->maparray[i])
 		{
-			printf("%c",map->maparray[i][j]);
+			int j=0;
+			while (map->maparray[i][j])
+			{
+				printf("%c",map->maparray[i][j]);
 				j++;
+			}
+			i++;
+			printf("\n");
 		}
-		i++;
-		printf("\n");
 	}
 	
-}*/
-
+}
