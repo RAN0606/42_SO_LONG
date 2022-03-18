30bbd9c4d32aa30b20127100c0051c1752799400
Ran LIU <rliu@e2r12p4.clusters.42paris.fr>
2022-03-18 21:15:32 +0100

    soir 0318

--- init_image.c
+++ init_image.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* ************************************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   init_image.c                                       :+:      :+:    :+:   */
@@ -6,71 +6,112 @@
 /*   By: rliu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 16:32:14 by rliu              #+#    #+#             */
-/*   Updated: 2022/03/18 15:33:24 by rliu             ###   ########.fr       */
+/*   Updated: 2022/03/18 21:09:18 by rliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "so_long.h"
-#include "mlx.h"
 typedef struct s_img
 {
 	void	*ptr;
-	int		*w; //width of img
-	int		*h;//height of img
+	int	w; //width of img
+	int	h;//height of img
 } t_img;
 
 typedef struct s_img_set
 {
 	t_img	wall;
-	t_img	floor;
-	t_img	collcet;
+	t_img	terrain;
+	t_img	collect;
 	t_img	exit;
 	t_img	start;
-	t_img	spirit;
+	t_img	player;
 } t_img_set;
 
 typedef struct s_play
 {
     void    *mlx_self;
     void    *mlx_win;
-	t_img_set	imgs;
-	int		x;
-	int		y;
+    t_img_set	imgs;
+    int		x;
+    int		y;
 } t_play;
 
-void ft_init_image(t_play play)
-{	
-	char	path[] = "img_48/Purple.xpm";
-	play.imgs.wall.ptr = mlx_xpm_file_to_image(play.mlx_self, path, play.imgs.wall.w, play.imgs.wall.h);	
-/*	play.imgs.floor.ptr = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));	
-	play.imgs.collect.ptr = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));	
-	play.imgs.exit.ptr = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));	
-	play.imgs.start.ptr = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));		
-	play.imgs.spirit.ptr = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));*/
+t_img ft_init_imgs_spirit(void *mlx, char *path)
+{		
+	t_img	img;
+	 
+	img.ptr = mlx_xpm_file_to_image(mlx, path, &(img.w), &(img.h));
+	return (img);	
 }
 
-int	main(void)
+t_img_set ft_init_imgs(void *mlx)
+{
+	t_img_set imgs;
+
+	imgs.wall = ft_init_imgs_spirit(mlx,"img_48/tree.xpm");
+	imgs.terrain = ft_init_imgs_spirit(mlx,"img_48/floor_purple.xpm");
+	imgs.collect = ft_init_imgs_spirit(mlx,"img_48/collect2.xpm");
+	imgs.exit = ft_init_imgs_spirit(mlx,"img_48/exit.xpm");
+	imgs.start = ft_init_imgs_spirit(mlx,"img_48/start.xpm");
+	imgs.player = ft_init_imgs_spirit(mlx,"cat_48/down_2.xpm");
+	return (imgs);
+
+}
+
+void ft_putimg(t_play play, t_map map, int line, int col)
+{
+	mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.terrain.ptr, col*48, line*48);
+	if (line == 0 || line == map.mapline - 1 || col == 0 || col == map.mapcol - 1)
+		mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.wall.ptr, col*48, line*48);
+	if (map.maparray[line][col] == 'P')
+		mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.player.ptr, col*48, line*48);
+	if (map.maparray[line][col] == 'C')
+		mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.collect.ptr, col*48, line*48);
+	if (map.maparray[line][col] == 'E')
+		mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.exit.ptr, col*48, line*48);
+
+
+}
+void ft_putimgs(t_play play, t_map map)
+{
+        int line = 0;
+        int col = 0;
+        while (line < map.mapline)
+        {
+                col = 0;
+                while (col < map.mapcol)
+                {
+			ft_putimg(play,map,line,col);
+                        col++;
+                }
+                line++;
+        }
+}
+
+void ft_init_display(t_play *play, t_map *map)
+{
+	int screen_l;
+	int screen_h;
+
+	play->mlx_self = mlx_init();
+	mlx_get_screen_size(play->mlx_self, &screen_l, &screen_h);
+	if (map->mapcol * 48 > screen_l || map->mapline * 48 > screen_h)
+	{
+		ft_map_exit("mapsize is too big for this screen!", map);
+	}
+	play->mlx_win = mlx_new_window(play->mlx_self, map->mapcol * 48, map->mapline * 48, "./so_long");
+	play->imgs = ft_init_imgs(play->mlx_self);	
+	
+}
+
+int	main(int argc, char **argv)
 {
 	t_play	play;
-//	t_map	map;
-//	char	relative_path1[] = "../sprit_cat/down_2.xpm";
-//	char	relative_path2[] = "../img/Jump.xpm";
-
-	play.x = 0;
-	play.y = 0;
-	play.mlx_self = mlx_init();
-	ft_init_image(play);
-//	play.img1 = mlx_xpm_file_to_image(play.mlx_self, relative_path1, &(play.img_width1), &(play.img_height1));
-//	play.img2 = mlx_xpm_file_to_image(play.mlx_self, relative_path2, &(play.img_width2), &(play.img_height2));	
-//	win_x = (play.img_width-1) * 9;
-//	win_y = (play.img_height-1) * 4;
-	play.mlx_win = mlx_new_window(play.mlx_self, 900, 400, "so_long");
-	mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.imgs.wall.ptr, 0, 0);	
-//	mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.img2, 0, 0);
-
-//	printf("size: %d", mlx_get_screen_size(play.mlx_self, &win_xi, &win_y));
-	//mlx_put_image_to_window(play.mlx_self, play.mlx_win, play.img, play.x, play.y);
-//	ft_putimg_set(play);
-//	mlx_key_hook(play.mlx_win, ft_keyhook, &play);
-//	mlx_mouse_get_pos(mlx, mlx_win, &img_width, &img_height);	
+	t_map	map;
+	
+	ft_checkmap(&map,argv[1]);
+	ft_init_display(&play,&map);
+	ft_putimgs(play,map);
 	mlx_loop(play.mlx_self);
 }
